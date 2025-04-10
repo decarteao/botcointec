5f4fd5497a8e75cf503d69bec75232bfaa2b3ac0
Decarte <decartecussenha@gmail.com>
2025-04-10 15:22:52 +0100

    make it better

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"botcointec/config"
+	"botcointec/controllers"
 	"botcointec/models"
 	"botcointec/utils"
 	"net/http"
@@ -16,6 +17,13 @@ type LoginJSON struct {
 	Password   string `json:"password"`
 	Code       string `json:"code"` // codigo do google recaptcha
 	RememberMe bool   `json:"remember_me"`
+}
+type RegisterJSON struct {
+	FullName string `json:"fullname"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"` // codigo do google recaptcha
+	Code     string `json:"code"`  // codigo do google recaptcha
 }
 
 func RegisterApi(r *gin.Engine, db *config.DataBase) {
@@ -72,4 +80,49 @@ func RegisterApi(r *gin.Engine, db *config.DataBase) {
 			}
 		}
 	})
+
+	// registro
+	api.POST("/register", func(ctx *gin.Context) {
+		var rj RegisterJSON
+
+		// passar o json do cliente pra variavel do servidor
+		ctx.BindJSON(&rj)
+
+		var user models.User
+
+		// pesquisar no db
+		if err := db.DB.Where("username = ? or email = ?", rj.Username, rj.Email).First(&user).Error; err != nil {
+			// Cadastrar user
+			controllers.AddUser(db, rj.FullName, rj.Username, rj.Email, rj.Password, "user")
+
+			// Buscar o user dnv
+			err = db.DB.Where("username = ? or email = ?", rj.Username, rj.Email).First(&user).Error
+
+			// Gerar token e redirecionar o usuario
+			token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+				"user_id":    user.ID,
+				"expiration": time.Now().Add(time.Hour * 168).Local().Unix(), // 7 dias
+				"role":       user.Role,
+				"remember":   false,
+			})
+
+			tokenString, _ := token.SignedString(utils.JwtSecrect)
+
+			// setar os cookies
+			ctx.SetCookie("token", tokenString, 604800, "/", "", false, true) // cookies validos ate 7 dias
+
+			// enviar pro cliente
+			ctx.JSON(http.StatusOK, gin.H{
+				"status": true,
+				// "token":       tokenString,
+				"redirect_to": "/dashboard",
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status": false,
+				"error":  "Username ou email já existente",
+			})
+		}
+	})
+
 }
